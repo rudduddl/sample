50419c563f0f1ea505b4a9d6922d8e7b6f23b586
rudduddl <kim5472678@gmail.com>
2024-09-03 13:56:36 +0900

    add App.java and App.class

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!!");
         System.out.println("hihihi");
+        System.out.println("asdfasdf");
     }
 }
